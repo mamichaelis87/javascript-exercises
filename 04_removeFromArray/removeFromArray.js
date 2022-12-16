bb0d03804273f5232d4bf26b492df0c52b9886cb
mamichaelis87 <mallenm1993@gmail.com>
2022-12-16 13:29:48 -0500

    Remove unnecessary comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,11 @@
 const removeFromArray = function(list, ...itemsToRemove) {
 
-   //or(let i = 0; i < list.length; i++){
+   
         for (let j = 0; j < itemsToRemove.length; j++){
            list = list.filter(listItem => listItem !== itemsToRemove[j]);
            
-            //if (list[i] === itemsToRemove[j]){
-           //     list.splice(i,1);
-            //}
         }
-    //}
+    
     return list;
 
 };
