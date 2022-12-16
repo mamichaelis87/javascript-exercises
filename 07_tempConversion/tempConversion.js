eaf55507b69105b9929f7bab54281bf01710aee9
mamichaelis87 <mallenm1993@gmail.com>
2022-12-16 13:10:08 -0500

    Complete tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    celcius = (temp - 32)*(5/9);
+    return Math.round(10*celcius)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    farenheit = temp*(9/5) + 32;
+    return Math.round(10*farenheit)/10;
+
 };
 
 // Do not edit below this line
