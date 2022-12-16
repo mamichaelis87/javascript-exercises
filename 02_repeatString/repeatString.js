bd48b6f53e5c114ab0588c93d75cca73ccf73e3c
mamichaelis87 <mallenm1993@gmail.com>
2022-12-15 19:22:08 -0500

    Complete function to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    let list = "";
+   
+    if (num < 0){
+        return "ERROR";
+    }
+   
+    else{
+    for (i = 0; i < num; i++){
+        list += string;
+        }
+    return list;}
 };
 
 // Do not edit below this line
