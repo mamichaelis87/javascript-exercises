23978e4f83093f35e983f892ce14733768d1f315
mamichaelis87 <mallenm1993@gmail.com>
2022-12-16 12:11:08 -0500

    Complete sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (typeof(a)!== "number" || typeof(b) !== "number") {return "ERROR"}
+    if (a < 0 || b < 0) {return "ERROR"}
+    
+    let small;
+    let large;
+    let sum = 0;
 
+    if (a<=b) {
+        small = a;
+        large = b;
+    }
+    else {
+        small = b;
+        large = a;
+    }
+
+    for (let i = small; i <= large; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
