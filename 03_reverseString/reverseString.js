d5d5523b181601889238b92ab6e8f54adada3125
mamichaelis87 <mallenm1993@gmail.com>
2022-12-15 19:35:25 -0500

    Complete reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reverseArray = [];
+    for (let i = 0; i < stringArray.length; i++){
+        reverseArray[i] = stringArray [(stringArray.length-1) - i];
+    }
+    return reverseArray.join('');
 };
 
 // Do not edit below this line
